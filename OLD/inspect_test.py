1c819c66210a463739a97246fcae16b4f7a03516
Preston Watts <prestonsw2014@gmail.com>
2020-12-27 17:44:18 -0600

    Work done

---
+++ OLD/inspect_test.py
@@ -0,0 +1,10 @@
+#inspect 
+import inspect 
+import json
+import helper
+
+print(inspect.signature(inspect.signature))
+
+print('skip') 
+
+print(inspect.signature(helper.askForChoice))
---
+++ cl.bat
@@ -0,0 +1,3 @@
+cd C:\Users\prest\OneDrive\Desktop\Learning\Areas of Interest\Programming, Etc\Python\CoverLetters\CoverLetter
+
+python 01_CL.py
